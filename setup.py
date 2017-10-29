232ad4d7dbfb2595ee7f18e3c215b1405d89a5b5
davistodt <davis.todt@roche.com>
2017-10-29 11:33:23 +0200

    Setup requirements and package structure

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+setuptools.setup(
+    name='database-alchemy',
+    version='0.1.0',
+    url='https://github.com/16967143/database-alchemy',
+
+    author='Nonchalant',
+    author_email='davistodt@hotmail.com',
+
+    description='Learning databases using sqlalchemy, sqlite3 and alembic for unknown reasons',
+    long_description=open('README.md').read(),
+
+    packages=['database-alchemy'],
+
+    install_requires=[
+        'click',
+        'pandas',
+    ],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    entry_points={
+        'console_scripts': [
+           # 'create-db = database-alchemy.create_db:main',
+           # 'modify-db = database-alchemy.modify_db:main',
+        ],
+    },
+)
